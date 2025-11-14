20fde5002cfa6a6d42c0f7c502267b0b736b8d1a
inigoggrijalba-lgtm <inigoggrijalba@gmail.com>
2025-11-15 00:49:49 +0100

    Initial project setup from scratch

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Establece la ruta base a '' para usar rutas relativas.
+  // Establece la ruta base al nombre de tu repositorio.
   // Esto asegura que los archivos se carguen correctamente en GitHub Pages.
-  base: '',
+  base: '/PorraGP-FULL-2025/',
   define: {
     // Esto hace que la variable de entorno del proceso de compilación
     // esté disponible en el código del navegador.
